f92d4087f2a0905dfcd3e7a4dde6ea4ec92c2145
annxeea <161916530+annxeea@users.noreply.github.com>
2024-03-02 14:11:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const html = `
             } 
 
             .button{
-                background-color: blue;
+                background-color: #00ABE9;
                 padding: 15px 30px;
                 border-radius: 5px;
                 cursor: pointer;
